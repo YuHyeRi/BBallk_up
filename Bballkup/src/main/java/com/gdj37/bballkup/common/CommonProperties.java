77bcb82be4dc33e054cf77937ac95c51c413e33f
USER <USER@DESKTOP-PC>
2021-11-02 20:46:52 +0900

    지나 업로드 파일

--- Bballkup/src/main/java/com/gdj37/bballkup/common/CommonProperties.java
+++ Bballkup/src/main/java/com/gdj37/bballkup/common/CommonProperties.java
@@ -20,7 +20,7 @@ public class CommonProperties {
 	 * 파일 업로드
 	 */
 	//파일 업로드 경로
-	public static final String FILE_UPLOAD_PATH = "D:\\workspace\\.metadata\\"
+	public static final String FILE_UPLOAD_PATH = "F:\\workspace\\.metadata\\"
 			+ ".plugins\\org.eclipse.wst.server.core\\tmp0\\"
 			+ "wtpwebapps\\Bballkup\\resources\\upload";
 	
